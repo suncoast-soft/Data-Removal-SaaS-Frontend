0cfede7fd909da3c3b4a92b4366b92d7caab814f
Dewayne Johnson <dj@suncoast-software.com>
2024-09-30 03:56:29 -0400

    chore: price type

--- utils/supabase/admin.ts
+++ utils/supabase/admin.ts
@@ -49,7 +49,9 @@ const upsertPriceRecord = async (
     unit_amount: price.unit_amount ?? null,
     interval: price.recurring?.interval ?? null,
     interval_count: price.recurring?.interval_count ?? null,
-    trial_period_days: price.recurring?.trial_period_days ?? TRIAL_PERIOD_DAYS
+    trial_period_days: price.recurring?.trial_period_days ?? TRIAL_PERIOD_DAYS,
+    description: '',
+    metadata: price.metadata || {}
   }
 
   const { error: upsertError } = await supabaseAdmin
