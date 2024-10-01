9f55f04a7f4b5691af3440b1a02cb3d85f6cd7c2
Dewayne Johnson <dj@suncoast-software.com>
2024-10-01 16:26:56 -0400

    chore: footer

--- components/modules/AccountForms/SubscriptionForm.tsx
+++ components/modules/AccountForms/SubscriptionForm.tsx
@@ -1,25 +1,70 @@
 'use client'
 
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
 import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { toast } from '@/hooks/use-toast'
+
+const FormSchema = z.object({
+  email: z.string().min(2, {
+    message: 'Email must be at least 2 characters.'
+  })
+})
 
 export default function SubscriptionForm() {
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: ''
+    }
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: 'You submitted the following values:',
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      )
+    })
   }
 
   return (
-    <form id="emailForm" onSubmit={(e) => handleSubmit(e)}>
-      <Input type="email" placeholder="Your Email Address" />
-      <Button
-        variant="default"
-        color="white"
-        type="submit"
-        form="emailForm"
-        className="w-full mt-1"
-      >
-        Subscribe
-      </Button>
-    </form>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type="email"
+                  placeholder="Your email address"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">
+          Submit
+        </Button>
+      </form>
+    </Form>
   )
 }
