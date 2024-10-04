a1a5f51569f280966fd09a54939c21899aeb0079
Dewayne Johnson <dj@suncoast-software.com>
2024-10-04 09:38:24 -0400

    feat: blank pages

---
+++ app/dashboard/support/page.tsx
@@ -0,0 +1,16 @@
+export default async function Support() {
+  return (
+    <section className="mb-16 bg-white">
+      <div className="max-w-6xl px-4 py-8 mx-auto sm:px-6 lg:px-8">
+        <div className="sm:align-center sm:flex sm:flex-col">
+          <h1 className="text-4xl font-extrabold text-primary sm:text-center sm:text-6xl">
+            Support
+          </h1>
+          <p className="max-w-2xl m-auto mt-5 text-xl text-primary sm:text-center sm:text-2xl">
+            Coming Soon
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
