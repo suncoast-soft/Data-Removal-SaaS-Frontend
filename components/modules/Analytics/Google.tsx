fa7ea4712cf1c3516a55380d56fd5c73eb0dd9f2
Dewayne Johnson <dj@suncoast-software.com>
2024-10-25 16:21:05 -0400

    chore: google search style

--- components/modules/Analytics/Google.tsx
+++ components/modules/Analytics/Google.tsx
@@ -1,9 +1,14 @@
 'use client'
 
 import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
+import Link from 'next/link'
 
 interface GoogleSearchResult {
   link: string
+  htmlTitle: string
+  htmlSnippet: string
+  displayLink: string
+  htmlFormattedUrl: string
 }
 
 export default function GoogleReport({
@@ -17,15 +22,20 @@ export default function GoogleReport({
         {results.map((search: GoogleSearchResult, index) => (
           <TableRow key={index}>
             <TableCell>
-              <ul>
-                {Object.entries(search)
-                  .filter(([key, value]) => value)
-                  .map(([key, value]) => (
-                    <li key={key} className="list-none">
-                      <strong>{key}:</strong> {String(value)}
-                    </li>
-                  ))}
-              </ul>
+              <p className="font-semibold text-slate-600">
+                {search.displayLink}
+              </p>
+              <Link href={search.link} className="no-underline mb-2">
+                <p
+                  dangerouslySetInnerHTML={{ __html: search.htmlTitle }}
+                  className="text-primary font-semibold text-lg"
+                />
+                <p
+                  dangerouslySetInnerHTML={{ __html: search.htmlFormattedUrl }}
+                  className="text-primary text-sm underline"
+                />
+              </Link>
+              <p dangerouslySetInnerHTML={{ __html: search.htmlSnippet }} />
             </TableCell>
           </TableRow>
         ))}
