363c654e3679a0abb26c808906b50071d4b12b66
Dewayne Johnson <dj@suncoast-software.com>
2024-11-13 03:01:10 -0500

    chore: enable rls for django tables

---
+++ supabase/migrations/20241113075935_enable-rls-django-tables.sql
@@ -0,0 +1,22 @@
+alter table "public"."auth_group" enable row level security;
+
+alter table "public"."auth_group_permissions" enable row level security;
+
+alter table "public"."auth_permission" enable row level security;
+
+alter table "public"."auth_user" enable row level security;
+
+alter table "public"."auth_user_groups" enable row level security;
+
+alter table "public"."auth_user_user_permissions" enable row level security;
+
+alter table "public"."django_admin_log" enable row level security;
+
+alter table "public"."django_content_type" enable row level security;
+
+alter table "public"."django_migrations" enable row level security;
+
+alter table "public"."django_session" enable row level security;
+
+
+
