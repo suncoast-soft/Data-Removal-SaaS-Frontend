b8c33d8752e5492bf39af4774c07ef0b81f3fb45
Dewayne Johnson <dj@suncoast-software.com>
2025-01-22 12:13:37 -0500

    fix: supabase migration

--- types_db.ts
+++ types_db.ts
@@ -339,30 +339,30 @@ export type Database = {
         Row: {
           created_at: string
           id: number
-          profile_id: number | null
           type: Database["public"]["Enums"]["pricing_type"] | null
           updated_at: string | null
+          user_id: string | null
         }
         Insert: {
           created_at?: string
           id?: number
-          profile_id?: number | null
           type?: Database["public"]["Enums"]["pricing_type"] | null
           updated_at?: string | null
+          user_id?: string | null
         }
         Update: {
           created_at?: string
           id?: number
-          profile_id?: number | null
           type?: Database["public"]["Enums"]["pricing_type"] | null
           updated_at?: string | null
+          user_id?: string | null
         }
         Relationships: [
           {
-            foreignKeyName: "pricing_plans_profile_id_fkey"
-            columns: ["profile_id"]
+            foreignKeyName: "plans_user_id_fkey"
+            columns: ["user_id"]
             isOneToOne: false
-            referencedRelation: "profiles"
+            referencedRelation: "users"
             referencedColumns: ["id"]
           },
         ]
@@ -540,15 +540,7 @@ export type Database = {
           id?: string
           stripe_customer_id?: string | null
         }
-        Relationships: [
-          {
-            foreignKeyName: "users_id_fkey"
-            columns: ["id"]
-            isOneToOne: true
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          },
-        ]
+        Relationships: []
       }
     }
     Views: {
@@ -654,5 +646,20 @@ export type Enums<
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
     ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof PublicSchema["CompositeTypes"]
+    | { schema: keyof Database },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
+    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
     : never
 
