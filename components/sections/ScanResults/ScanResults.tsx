22310692712ef10abef28a6c72b2839468a0aa64
Dewayne Johnson <dj@suncoast-software.com>
2025-02-03 01:42:37 -0500

    fix: data fetch to client

--- components/sections/ScanResults/ScanResults.tsx
+++ components/sections/ScanResults/ScanResults.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
 import UpgradeSection from '@/components/sections/Dashboard/UpgradeSection'
 import HowToProtectSection from '@/components/sections/Dashboard/HowToProtectSection'
@@ -8,8 +10,15 @@ import PrivateFAQs from '@/components/sections/PrivateFAQs'
 import SearchSummary from '@/components/sections/SearchReport/SearchSummary'
 import SearchResults from '@/components/sections/SearchReport/SearchResults'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
+import { Tables } from '@/types_db'
 
-export default async function ScanResults({
+type BrokerSearch = Tables<'broker_searches'> & {
+  broker: Tables<'brokers'>
+}
+
+export default function ScanResults({
   name,
   city,
   state
@@ -20,15 +29,61 @@ export default async function ScanResults({
 }) {
   const { firstName, lastName } = splitName(name ?? '')
 
-  const googleResponse = await fetch(
-    `https://api.puperase.com/api/check?type=google&first_name=${firstName}&last_name=${lastName}&city=${city}&state=${state}`
-  )
-  const googleSearches = [{ search_result: await googleResponse.json() }]
+  const [loading, setLoading] = useState(true)
+  const [googleSearches, setGoogleSearches] = useState<any[]>([])
+  const [brokerSearches, setBrokerSearches] = useState<BrokerSearch[]>([])
 
-  const brokerResponse = await fetch(
-    `https://api.puperase.com/api/check?type=broker&first_name=${firstName}&last_name=${lastName}&city=${city}&state=${state}`
-  )
-  const brokerSearches = await brokerResponse.json()
+  useEffect(() => {
+    async function fetchIPAPI() {
+      try {
+        const googleResponse = await fetch(
+          `https://api.puperase.com/api/check?type=google&first_name=${firstName}&last_name=${lastName}&city=${city}&state=${state}`
+        )
+        const googleData = await googleResponse.json()
+        setGoogleSearches([{ search_result: googleData }])
+
+        const brokerResponse = await fetch(
+          `https://api.puperase.com/api/check?type=broker&first_name=${firstName}&last_name=${lastName}&city=${city}&state=${state}`
+        )
+        const brokerData = await brokerResponse.json()
+        setBrokerSearches(brokerData)
+      } catch (error) {
+        console.log(error)
+      }
+
+      setLoading(false)
+    }
+    fetchIPAPI()
+  }, [firstName, lastName, city, state])
+
+  if (loading) {
+    return (
+      <div className="bg-lp-hero-section-bg bg-cover bg-bottom fixed inset-0 z-[9999] text-dark h-screen">
+        <div className="container mx-auto px-4 lg:px-28 h-full flex items-center justify-center">
+          <div className="text-center max-w-lg flex flex-col items-center gap-6">
+            <Image
+              src="/loaders/hero-image.png"
+              width={309}
+              height={358}
+              alt="Report Loader"
+              className="order-2 lg:order-1"
+            />
+            <div className="order-1 lg:order-2">
+              <h1 className="text-2xl lg:text-5xl font-bold">
+                Generating your report
+              </h1>
+              <p className="mt-4 text-lg lg:text-xl opacity-60">
+                This will only take a second
+              </p>
+              <p className="mt-4 text-lg lg:text-xl font-semibold">
+                Searching: ...{name}...
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <>
