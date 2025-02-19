c32a6e293634f105f8e2e6d2e4790b14d1cca634
Dewayne Johnson <dj@suncoast-software.com>
2025-02-19 17:27:12 -0500

    fix: build error

--- app/(public)/blog/page.tsx
+++ app/(public)/blog/page.tsx
@@ -2,7 +2,7 @@ import { sanityClient } from '@/utils/sanity/lib/client'
 import { Page } from '@/sanity.types'
 import RenderSanitySections from '@/components/sections/RenderSanitySections'
 
-export default async function HomePage() {
+export default async function BlogPage() {
   const data = await sanityClient.fetch(
     `*[_type == "page" && slug.current == $slug][0]`,
     { slug: 'blog' }
