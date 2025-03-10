9664e48869cf52692ce623fd19bfaafbad86a6dc
Dewayne Johnson <dj@suncoast-software.com>
2025-03-10 14:18:52 -0400

    fix: sanity data error

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,12 +1,17 @@
 import { sanityClient } from '@/utils/sanity/lib/client'
 import { Page } from '@/sanity.types'
 import RenderSanitySections from '@/components/sections/RenderSanitySections'
+import { notFound } from 'next/navigation'
 
 export default async function HomePage() {
   const data = await sanityClient.fetch(
     `*[_type == "page" && slug.current == $slug][0]`,
     { slug: 'home' }
   )
+
+  if (!data) {
+    return notFound()
+  }
 
   const { slug, content } = data as Page
 
